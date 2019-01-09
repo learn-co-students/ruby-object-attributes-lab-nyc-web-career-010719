ddadc85594f1ec3cc91cdaed7858c5f52deb4afc
github username <github email address>
2019-01-08 22:28:13 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  #2 methods. #name and #name= that read and write corresponding to the instance var @name
+  #2 methods. give a dog a breed. #breed & #breed=
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job =  job
+end
+end
